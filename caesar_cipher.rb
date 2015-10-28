36e4479df2af51bdbe4b88dd7b7e8c7897c26209
Sharon Siegel <acodeinprogress@KMMacBook.local>
2015-10-28 12:12:59 -0600

    Changed while loop to for loop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -7,15 +7,14 @@ class String
     alphabet = ('A'..'Z').to_a
     str = self.split("")
     i = 0
-    while i < str.length
-      if str[i] == " "
+    str.each_with_index do |item, i|
+      if item == " "
         str[i] = " "
       else
-        value = alphabet.index(str[i].upcase)
+        value = alphabet.index(item.upcase)
         value > 11 ? value -= 13 : value += 13
-        str[i] == str[i].downcase ? str[i] = alphabet[value].downcase : str[i] = alphabet[value]
+        str[i] == item.downcase ? str[i] = alphabet[value].downcase : str[i] = alphabet[value]
       end
-      i += 1
     end
     return str.join("")
 	end
